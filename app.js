86fdd8cfb833482069c914e012e26f98076e82ea
Lucky Singh <luckykumar60621@gmail.com>
2026-04-21 08:32:04 +0530

    add files

---
+++ app.js
@@ -0,0 +1,43 @@
+addEventListener("load", function () {
+    setTimeout(hideURLbar, 0);
+}, false);
+
+function hideURLbar() {
+    window.scrollTo(0, 1);
+}
+
+function showFeePage() {
+    document.getElementById("payment-container").style.display = "block";
+}
+
+// stats
+const counters = document.querySelectorAll('.counter');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const counter = entry.target;
+
+            const updateCount = () => {
+                const target = +counter.getAttribute('data-target');
+                const count = +counter.innerText;
+
+                const increment = target / 100;
+
+                if (count < target) {
+                    counter.innerText = Math.ceil(count + increment);
+                    setTimeout(updateCount, 20);
+                } else {
+                    counter.innerText = target + "+";
+                }
+            };
+
+            updateCount();
+            observer.unobserve(counter);
+        }
+    });
+}, { threshold: 0.5 });
+
+counters.forEach(counter => {
+    observer.observe(counter);
+});
